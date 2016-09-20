bf0355976f7ddf96a8726f947bf6699dfaf742c7
Darshil Patel <darshilpatel@users.noreply.github.com>
2016-09-20 11:39:26 -0400

    Add files via upload

---
+++ J2/J2A6/J2A6.java
@@ -0,0 +1,33 @@
+//**********************************************************
+// Assignment: J2A6
+//
+// Description: Concatentation and Intro to Variables
+// Author: Darshil Patel
+//
+// Date Start: SEPT 20, 2016
+// Date Completed: SEPT 20, 2016
+//
+// Completion time: 10 mins
+
+// Honor Code: I pledge that this program represents my own
+//   program code. I received help from (enter the names of
+//   others that helped with the assignment; write no one if
+//   you received no help) in designing and debugging my program.
+//*********************************************************
+
+public class J2A6 {
+   public static void main(String[] args) {
+    int numberA = 12;
+    int numberB = 9; 
+    int addition = numberA+numberB;
+    int substraction = numberA-numberB;
+    int product = numberA*numberB;
+    int quotient = numberA/numberB;
+    
+    System.out.println("The sum of 12 plus 9 is " + addition);
+     System.out.println("The difference 12 minus 9 is " + substraction);
+     System.out.println("The product  of 12 times 9 is " + product);
+     System.out.println(" The quotient of 12 divided by 9 is " + quotient);
+    
+         }
+}
